ab586713e558e3684eb25924abfd70aa035a2ce5
Nicholas Hoang <nicholash489@gmail.com>
2022-10-26 02:00:57 -0500

    starting tabs

---
+++ src/components/Tabs/TabMaster.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./TabMaster.css";
+
+function TabMaster() {
+  const [active, setActive] = useState(1);
+
+  return <div></div>;
+}
+
+export default TabMaster;
