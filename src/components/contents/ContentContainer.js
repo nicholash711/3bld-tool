4ae87275a42d848e53346d77fea00564048c8056
Nicholas Hoang <nicholash489@gmail.com>
2022-12-25 02:41:25 -0600

    organizing

--- src/components/contents/ContentContainer.js
+++ src/components/contents/ContentContainer.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./ContentContainer.css";
-import PracticeTab from "./PracticeTab";
-import InputTab from "./InputTab";
-import MemoTab from "./MemoTab";
-import OptionsTab from "./OptionsTab";
+import PracticeTab from "./Practice/PracticeTab";
+import InputTab from "./Input/InputTab";
+import MemoTab from "./Memo/MemoTab";
+import OptionsTab from "./Options/OptionsTab";
 
 export default function ContentContainer({ curTab }) {
   const content = [<PracticeTab />, <MemoTab />, <InputTab />, <OptionsTab />];
