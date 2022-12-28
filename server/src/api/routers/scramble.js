b1472b62644c4acda35b8c807db50d77ce17bd58
Nicholas Hoang <nicholash489@gmail.com>
2022-12-28 03:05:00 -0600

    removing scramble from backend

--- server/src/api/routers/scramble.js
+++
@@ -1,8 +0,0 @@
-import { randomScramblefromEvent } from "cubing";
-
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send({ scramble: randomScramblefromEvent("333bf") });
-});
