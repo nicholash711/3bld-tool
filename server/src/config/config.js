8cb7655f991f866bd1f984cb854337b63dcc732d
nicholash711 <nicholash489@gmail.com>
2023-05-14 15:46:10 -0500

    renamed file

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,3 @@
-// require("dotenv").config();
-
 const config = {
   port: process.env.PORT || 5000,
   db_url: process.env.MONGODB_URL || "mongodb://localhost:27017",
--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const config = require("./config/settings.js");
+const config = require("./config/config.js");
 
 // import routes
 const solves = require("./api/routers/solves");
